50df4c544f06152d89ef2fb73f93234a45537956
Omar Vega <omarvega598@gmail.com>
2022-11-10 13:05:03 -0500

    add new data to user

--- src/api/user/user.model.js
+++ src/api/user/user.model.js
@@ -14,8 +14,16 @@ const userSchema = new Schema(
       type: String,
       required: true,
     },
+    fullName: String,
+    adress: String,
+    city: String,
+    state_province_region: String,
+    zip: String,
+    country: String,
+    phoneNumber: String,
     products: [{ type: Schema.Types.ObjectId, ref: 'Product' }],
   },
+
   { timestamps: true }
 );
 const User = model('User', userSchema);
--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ const { connect } = require('./database');
 
 const app = express();
 
-app.listen(process.env.PORT || 3000, () => {
+app.listen(process.env.PORT || 8080, () => {
   expressConfig(app);
   connect();
   routesConfig(app);
